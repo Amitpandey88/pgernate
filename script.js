a02a1289483e9f34d9a3717a10a775ed7a991fa0
Amitpandey88 <70188972+Amitpandey88@users.noreply.github.com>
2023-03-28 15:02:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ function generatePassword() {
 }
 
 document.getElementById('generate-btn').addEventListener('click', generatePassword);
+
+// JavaScript for pop-up
+window.onload = function() {
+  var popupWrapper = document.querySelector('.popup-wrapper');
+  var closePopup = document.querySelector('#close-popup');
+  
+  if(!sessionStorage.getItem('popupDisplayed')) {
+    popupWrapper.style.display = 'flex';
+    sessionStorage.setItem('popupDisplayed', 'true');
+  }
+  
+  closePopup.addEventListener('click', function() {
+    popupWrapper.style.display = 'none';
+  });
+}
